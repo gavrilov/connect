6d2a7d1aa8335706fe99d487864fd1a4499c226a
Konstantin Gavrilov <gavrilovks@gmail.com>
2022-02-16 23:07:34 -0800

    add flask server

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host='0.0.0.0')
